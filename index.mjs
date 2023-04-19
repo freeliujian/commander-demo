50ad15bb2c81eaae2182b5f068a3bdbd0ddfc7a3
littleforest <670177821@qq.com>
2023-04-19 11:15:02 +0800

    feat: add API

--- index.mjs
+++ index.mjs
@@ -5,17 +5,36 @@ import pkg  from './package.json' assert { type: 'json' };
 
 program.version(pkg.version);
 
-program
-  .option('-t --test [testMessage...]', '测试注册指令', '测试注册指令')
-  .option('-tt|--testt [s]', '测试注册指令')
-  .option('--no-f --no-file', '测试返回值为false')
-  .requiredOption('-l --lli <value>', '必填指令','必须有值')
+const existHandle = (value) => {
+  console.log(value,prev)
+  const newValue = `111${value}`;
+  return newValue;
+}
 
+
+/**
+ * option() 注册指令
+ */
+/**
+ * requiredOption 注册指令，必填
+ */
+program
+  // .option('-t --test [testMessage...]', '测试注册指令', '测试注册指令')
+  .option('-tt|--testt [s]', '测试注册指令')
+  // .option('--no-f --no-file', '测试返回值为false')
+  // .requiredOption('-l --lli <value>', '必填指令', '必须有值')
+  .option('-eee --eeexist <asd>', '额外参数处理', existHandle)
+  .command('clone')
+  .argument('[account]')
+  .argument('<password>')
+  .action((account,password) => {
+    console.log(account,password)
+  })
 
 program.parse(process.argv);
 
 const options = program.opts();
-console.log(program.getOptionValue('test'));
+//program.getOptionValue('eeexist')
 console.log(options);
 
 
